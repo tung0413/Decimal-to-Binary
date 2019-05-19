74f8896b830213ff6336f8491bfcc2ee848e038b
tung0413 <46188799+tung0413@users.noreply.github.com>
2019-05-19 21:49:43 +0800

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,15 @@
 void convert(double num,int n)
 {
     if(num==1){n=10;printf("1");}
-    if(n<10){
-        if(num>1){
+    if(n<10)
+    {
+        if(num>1)
+        {
             if(n!=0)printf("1");
             convert((num-1)*2,n+1);
         }
-        else if(num<1){
+        else if(num<1)
+        {
             if(n!=0)printf("0");
             convert(num*2,n+1);
         }
